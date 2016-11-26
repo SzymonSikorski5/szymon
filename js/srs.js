77a33a02f0f4c58c7386993367d5825dde2544c5
SzymonSikorski5 <szymoniak3121@interia.pl>
2016-11-26 21:07:09 +0100

    Add files via upload

---
+++ js/srs.js
@@ -0,0 +1,6 @@
+angular.module('SrsApp', [])
+        .controller('SrsHead', function($scope) {
+            $scope.Index = "SRS - Strona główna";
+})
+		.controller('SrsNawigacja', function($scope) {
+});
