516f783dd20f6dca2b39c0a8229cbfcf286c5687
SzymonSikorski5 <szymoniak3121@interia.pl>
2016-11-27 00:16:50 +0100

    Add files via upload

---
+++ logowanie.php
@@ -0,0 +1,19 @@
+<html ng-app="SrsApp">
+    <head ng-controller="SrsHead">
+        <meta charset="UTF-8">
+        <title>{{Logowanie}}</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+		<script src="js/srs.js"></script>
+    </head>
+    <body>
+		<form>
+			<input type="text" id="" class="" name="email" required>
+			<input type="text" id="" class="" name="haslo" required>
+			<input type="submit" id="" class="" name="Zaloguj" value="Zaloguj">
+		</form>
+        <?php
+		
+        ?>
+    </body>
+</html>
