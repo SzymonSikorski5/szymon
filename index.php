e99bc7c924ba8f694de8e33dbb1425920bf33cb9
SzymonSikorski5 <szymoniak3121@interia.pl>
2016-11-26 21:07:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html ng-app="SrsApp">
+    <head ng-controller="SrsHead">
+        <meta charset="UTF-8">
+        <title>{{Index}}</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+		<script src="js/srs.js"></script>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
